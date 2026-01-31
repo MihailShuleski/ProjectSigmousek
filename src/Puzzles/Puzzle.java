4b01814d75163b3b160ce674bff1c6035b0901bb
Shul <shuleski@spsejecna.cz>
2026-01-31 14:25:29 +0100

    pridani logickych hadanek

--- src/Puzzles/Puzzle.java
+++ src/Puzzles/Puzzle.java
@@ -4,7 +4,7 @@ public class Puzzle {
     private String name;
     private String question;
     private String correctAnswer;
-    private String damage; //kazda hadanka bude delat jiny pocet damage ig
+    private int damage; //kazda hadanka bude delat jiny pocet damage ig
 
     public void ukazHadanku(){
         //TODO metoda pro ukazovani hadanky
