5594613e85cb4c70daf3c898988451205b3322f1
Shul <shuleski@spsejecna.cz>
2026-01-31 17:25:18 +0100

    pridani zaklad PouzijCommand a ProzkoumejCommand

--- src/Command/PouzijCommand.java
+++ src/Command/PouzijCommand.java
@@ -6,7 +6,13 @@ public class PouzijCommand implements Command{
     Spongebob spongebob=new Spongebob();
     @Override
     public void execute(String[] parts) {
-        return;
+        if (parts.length<2){
+            System.out.println("Musíš napsat co chceš použít(Ex. pouzij *item*");
+            return;
+        }
+        String Item=parts[1];
+        System.out.println("Uspěšně jsi použil předmět: "+Item);
+
     }
 
     @Override
--- src/Command/ProzkoumejCommand.java
+++ src/Command/ProzkoumejCommand.java
@@ -1,9 +1,19 @@
 package Command;
 
+import Characters.Spongebob;
+import Planets.Planet;
+
 public class ProzkoumejCommand implements Command{
+    Spongebob spongebob;
+    public ProzkoumejCommand(Spongebob spongebob){
+        this.spongebob=spongebob;}
     @Override
     public void execute(String[] parts) {
+        String planetId = spongebob.getCurrentPlanet();
+        Planet planet = spongebob.getData().findPlanet(planetId);
 
+        System.out.println("Prozkoumáváš planetu: " + planet.getName());
+        System.out.println(planet.getInfo());
     }
 
     @Override
