609922479d3d234960924d8b7726664ffbfa9138
shuleski <email@email.com>
2026-02-02 09:55:41 +0100

    Metoda pro nacitani hadanek ze souboru

--- src/Puzzles/Puzzle.java
+++ src/Puzzles/Puzzle.java
@@ -1,24 +1,9 @@
 package Puzzles;
 
 public class Puzzle {
-    private String id;
-    private String type;
     private String question;
     private String correctAnswer;
     private int damage; //kazda hadanka bude delat jiny pocet damage ig
-
-
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getType() {
-        return type;
-    }
-    public String getId() {
-        return id;
-    }
 
 
     public String getQuestion() {
@@ -33,36 +18,11 @@ public class Puzzle {
         return damage;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
 
-
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public void setCorrectAnswer(String correctAnswer) {
-        this.correctAnswer = correctAnswer;
-    }
-
-    public void setDamage(int damage) {
-        this.damage = damage;
-    }
-
-    public Puzzle(String id,String type, String question, String correctAnswer, int damage) {
-        this.id = id;
-        this.type=type;
+    public Puzzle(String question, String correctAnswer, int damage) {
         this.question = question;
         this.correctAnswer = correctAnswer;
         this.damage = damage;
     }
 
-    public void showPuzzle(){
-        //TODO metoda pro ukazovani hadanky
-    }
-    public void checkAnswer(){
-        //TODO pridat metodu pro kontrolovani odpoved
-    }
 }
---
+++ src/Puzzles/Text/PuzzleLoader.java
@@ -0,0 +1,34 @@
+package Puzzles.Text;
+
+import Puzzles.Puzzle;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class PuzzleLoader {
+    ArrayList<Puzzle> puzzles=new ArrayList<>(10);
+
+    public void loadPuzzle(String resourcePath){
+        String question;
+        String correctAnswer;
+        int damage;
+
+
+        try (BufferedReader br=new BufferedReader(new FileReader(resourcePath))) {
+            do {
+                question = br.readLine();
+                correctAnswer = br.readLine();
+                damage = Integer.parseInt(br.readLine());
+                puzzles.add(new Puzzle(question, correctAnswer, damage));
+            } while ((question = br.readLine()) != null);
+        } catch (FileNotFoundException e) {
+            System.out.println("Soubor " + resourcePath + " neexistuje");
+        } catch (IOException e) {
+            System.out.println("Problem se souborem: " + resourcePath);
+        }
+    }
+}
+
