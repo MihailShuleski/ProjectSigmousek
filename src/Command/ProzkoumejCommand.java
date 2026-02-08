cef74d4b98b2622ee230cc85ab455cc76e7d0720
Shul <shuleski@spsejecna.cz>
2026-02-08 17:21:06 +0100

    dodelavani ProzkoumejCommand

--- src/Command/ProzkoumejCommand.java
+++ src/Command/ProzkoumejCommand.java
@@ -1,19 +1,40 @@
 package Command;
 
 import Characters.Spongebob;
+import Items.Item;
 import Planets.Planet;
 
 public class ProzkoumejCommand implements Command{
     Spongebob spongebob;
-    public ProzkoumejCommand(Spongebob spongebob){
-        this.spongebob=spongebob;}
+    public ProzkoumejCommand(){
+    }
+    public void setSpongebob(Spongebob spongebob){
+        this.spongebob=spongebob;
+    }
+    private String normalizeInput(String input){
+        return input.trim().toLowerCase().replace(" ","_");
+    }
     @Override
     public void execute(String[] parts) {
-        String planetId = spongebob.getCurrentPlanet();
-        Planet planet = spongebob.getData().findPlanet(planetId);
+        Planet current=spongebob.getCurrentPlanet();
+        String planetId=parts[1];
+        for (int i= 2;i<parts.length;i++){ //pokud ma planeta vice jak 1 slovo treba Terra Prime tak se z toho udela string z tech dvou slov
+            planetId += " "+ parts[i];
+        }
+        String planet1=normalizeInput(planetId);
 
-        System.out.println("Prozkoumáváš planetu: " + planet.getName());
-        System.out.println(planet.getInfo());
+        System.out.println("Prozkoumáváš planetu: " + current.getName());
+        System.out.println(current.getInfo());
+        if (current.hasUnsolved()) {
+            System.out.println("Na planetě jsou furt hadanky");
+        }
+        if (!current.getItems().isEmpty()){
+            System.out.println("Vidíš itemy: ");
+            for (Item item:current.getItems()){
+                System.out.println("- "+current.getName());
+            }
+        }
+
     }
 
     @Override
